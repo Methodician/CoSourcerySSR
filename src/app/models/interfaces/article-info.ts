46e4dfa106780391b008958e8d43255c8dc2c6c0
Methodician <methodician@gmail.com>
2019-06-01 15:11:11 -0700

    Added some relevant models

--- src/app/models/interfaces/article-info.ts
+++ src/app/models/interfaces/article-info.ts
@@ -15,6 +15,34 @@ export interface ArticlePreview {
   isFlagged?: boolean;
 }
 
+export interface ArticleDetail {
+  articleId: string;
+  authorId: string;
+  title: string;
+  introduction: string;
+  body: string;
+  imageUrl: string;
+  imageAlt: string;
+  authorImageUrl: string;
+  lastUpdated: any;
+  timestamp: any;
+  lastEditorId: string;
+  version: number;
+  editors: KeyMap<number>;
+  commentCount?: number;
+  viewCount?: number;
+  tags?: string[];
+  isFeatured?: boolean;
+  isFlagged?: boolean;
+  bodyImages?: BodyImageMap;
+}
+
 export interface KeyMap<T> {
   [key: string]: T;
 }
+export interface BodyImageMeta {
+  orientation: number;
+  path: string;
+}
+
+export interface BodyImageMap extends KeyMap<BodyImageMeta> {}

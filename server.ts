e9a8bf31f6167c1c6f670662367ff48baf6b1c94
Methodician <methodician@gmail.com>
2019-06-09 16:39:46 -0700

    Updated server.ts for debug-agent Assuming this is where that code goes.

--- server.ts
+++ server.ts
@@ -4,9 +4,19 @@ import { enableProdMode } from '@angular/core';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+// Import gCloud debug-agent for Stackdriver debugging
+import * as debugAgent from '@google-cloud/debug-agent';
 
 import * as express from 'express';
 import { join } from 'path';
+
+// Enable gCould debug-agent
+debugAgent.start({
+  serviceContext: {
+    service: 'CoSourcery-dev',
+    version: 'v1.1',
+  },
+});
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -26,7 +36,7 @@ app.engine(
   ngExpressEngine({
     bootstrap: AppServerModuleNgFactory,
     providers: [provideModuleMap(LAZY_MODULE_MAP)],
-  }),
+  })
 );
 
 app.set('view engine', 'html');
@@ -39,7 +49,7 @@ app.get(
   '*.*',
   express.static(DIST_FOLDER, {
     maxAge: '1y',
-  }),
+  })
 );
 
 // All regular routes use the Universal engine

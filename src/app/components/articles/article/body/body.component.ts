0a7429bc7c1387554889833d36079f1739937eb1
Methodician <methodician@gmail.com>
2019-06-22 12:51:03 -0700

    POC working

--- src/app/components/articles/article/body/body.component.ts
+++ src/app/components/articles/article/body/body.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'cos-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit {
+  @Input() parentForm: FormGroup;
+  @Input() isActive: boolean;
+  @Input() body: string;
+  @Input() isLoggedIn: boolean;
 
-  constructor() { }
+  @Output() onCtrlToggle = new EventEmitter();
+  @Output() onClickOut = new EventEmitter();
 
-  ngOnInit() {
+  constructor() {
+    const { parentForm, isActive, body, onCtrlToggle, onClickOut } = this;
+    console.log({
+      parentForm,
+      isActive,
+      body,
+      onCtrlToggle,
+      onClickOut,
+    });
   }
 
+  ngOnInit() {
+    const { parentForm, isActive, body, onCtrlToggle, onClickOut } = this;
+    console.log({
+      parentForm,
+      isActive,
+      body,
+      onCtrlToggle,
+      onClickOut,
+    });
+  }
 }

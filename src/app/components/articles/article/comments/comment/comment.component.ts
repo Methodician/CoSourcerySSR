25ee4bc08df7ae38ecafc81cb288728cae1537e8
Methodician <methodician@gmail.com>
2019-06-23 13:57:27 -0700

    Created comment component and might be making comments

--- src/app/components/articles/article/comments/comment/comment.component.ts
+++ src/app/components/articles/article/comments/comment/comment.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '@services/user.service';
+import { UserInfo } from '@models/classes/user-info';
+import { Comment } from '@models/interfaces/comment';
 @Component({
   selector: 'cos-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
+  @Input() comment: Comment;
+  @Input() isBeingEdited = false;
 
-  constructor() { }
+  authorInfo: UserInfo;
+  constructor(private userSvc: UserService) {}
 
   ngOnInit() {
+    this.initializeAuthor();
   }
 
+  initializeAuthor = async () => {
+    const snap = await this.userSvc
+      .userRef(this.comment.authorId)
+      .query.once('value');
+    const author = snap.val();
+    this.authorInfo = new UserInfo(author);
+  };
 }

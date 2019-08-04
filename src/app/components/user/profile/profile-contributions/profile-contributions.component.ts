7ba55aa9e95ed1adf2d4078938c9d7c7a210b1f2
Methodician <methodician@gmail.com>
2019-08-04 15:13:39 -0700

    Component renders

---
+++ src/app/components/user/profile/profile-contributions/profile-contributions.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'cos-profile-contributions',
+  templateUrl: './profile-contributions.component.html',
+  styleUrls: ['./profile-contributions.component.scss'],
+})
+export class ProfileContributionsComponent implements OnInit {
+  @Input() profileId: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}

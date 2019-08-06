e41d67601e3d7b460422d388cadf587918e76355
Methodician <methodician@gmail.com>
2019-08-06 10:43:49 -0700

    How silly, that was not prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
-    authDomain: 'cosourcerytest.firebaseapp.com',
-    databaseURL: 'https://cosourcerytest.firebaseio.com',
-    projectId: 'cosourcerytest',
-    storageBucket: 'cosourcerytest.appspot.com',
-    messagingSenderId: '146479623747',
-    appId: '1:146479623747:web:048a48804bbfcc9b',
+    apiKey: 'AIzaSyD-0e29imJ2TI3N0Wen2njZdBESwOxI6kM',
+    authDomain: 'cosourcery.firebaseapp.com',
+    databaseURL: 'https://cosourcery.firebaseio.com',
+    projectId: 'cosourcery',
+    storageBucket: 'cosourcery.appspot.com',
+    messagingSenderId: '141292210727',
+    appId: '1:141292210727:web:56e5572ac2307685',
     //DEV
     // apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
     // authDomain: 'cosourcerytest.firebaseapp.com',

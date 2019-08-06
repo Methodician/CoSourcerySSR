6f76b3f89f4149e1af18f4dabf74e1f3af0eddc5
Methodician <methodician@gmail.com>
2019-08-06 10:42:44 -0700

    Switched prod back to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    // apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
-    // authDomain: 'cosourcerytest.firebaseapp.com',
-    // databaseURL: 'https://cosourcerytest.firebaseio.com',
-    // projectId: 'cosourcerytest',
-    // storageBucket: 'cosourcerytest.appspot.com',
-    // messagingSenderId: '146479623747',
-    // appId: '1:146479623747:web:048a48804bbfcc9b',
-    //DEV
     apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
     authDomain: 'cosourcerytest.firebaseapp.com',
     databaseURL: 'https://cosourcerytest.firebaseio.com',
@@ -16,5 +8,13 @@ export const environment = {
     storageBucket: 'cosourcerytest.appspot.com',
     messagingSenderId: '146479623747',
     appId: '1:146479623747:web:048a48804bbfcc9b',
+    //DEV
+    // apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
+    // authDomain: 'cosourcerytest.firebaseapp.com',
+    // databaseURL: 'https://cosourcerytest.firebaseio.com',
+    // projectId: 'cosourcerytest',
+    // storageBucket: 'cosourcerytest.appspot.com',
+    // messagingSenderId: '146479623747',
+    // appId: '1:146479623747:web:048a48804bbfcc9b',
   },
 };

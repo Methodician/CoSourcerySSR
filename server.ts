9130a2000e8b28c51756f9b7da3f3fe2040df0ea
Jacob Johnston <Methodician@tricias-mbp.lan>
2020-10-14 14:53:30 -0700

    ngUpdated ngUinversal, prod build still broken

--- server.ts
+++ server.ts
@@ -1,145 +1,60 @@
 import 'zone.js/dist/zone-node';
-import { enableProdMode } from '@angular/core';
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
+import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
 
-import axios from 'axios';
+import { AppServerModule } from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
 
-// Polyfills required for Firebase
-(global as any).XMLHttpRequest = require('xhr2');
-// may also come in handy
-// (global as any).WebSocket = require('ws');
+// The Express app is exported so that it can be used by serverless Functions.
+export function app() {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
-// Import gCloud debug-agent for Stackdriver debugging
-import * as debugAgent from '@google-cloud/debug-agent';
-// Enable gCould debug-agent
-debugAgent.start();
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
 
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
 
-// Express server
-const app = express();
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+  }));
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+  });
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
+  return server;
+}
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine(
-  'html',
-  ngExpressEngine({
-    bootstrap: AppServerModuleNgFactory,
-    providers: [provideModuleMap(LAZY_MODULE_MAP)],
-  }),
-);
+function run() {
+  const port = process.env.PORT || 4000;
 
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
 
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
 
-const generateSiteMap = async () => {
-  const lastmod = new Date().toISOString();
-  const slugsUrl = 'https://cosourcery.firebaseio.com/articleData/slugs.json';
-  const usersUrl = 'https://cosourcery.firebaseio.com/userInfo/open.json';
-
-  const responses = await axios.all([axios.get(slugsUrl), axios.get(usersUrl)]);
-  const articleSlugs = Object.keys(responses[0].data);
-  const userIds = Object.keys(responses[1].data);
-
-  let xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-          http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`;
-
-  const homeBlock = generateUrlBlock(
-    'https://cosourcery.com/',
-    lastmod,
-    0.75,
-    'daily',
-  );
-  xml += homeBlock;
-
-  for (let slug of articleSlugs) {
-    const block = generateUrlBlock(
-      `https://cosourcery.com/article/${slug}`,
-      lastmod,
-      1.0,
-      'monthly',
-    );
-
-    xml += block;
-  }
-
-  for (let id of userIds) {
-    const block = generateUrlBlock(
-      `https://cosourcery.com/profile/${id}`,
-      lastmod,
-      0.5,
-      'monthly',
-    );
-
-    xml += block;
-  }
-
-  xml += '</urlset>';
-  return xml;
-};
-
-const generateUrlBlock = (
-  loc: string,
-  lastmod: string,
-  priority: number,
-  changefreq:
-    | 'always'
-    | 'hourly'
-    | 'daily'
-    | 'weekly'
-    | 'monthly'
-    | 'yearly'
-    | 'never',
-) => {
-  let xml = '<url>';
-  xml += `<loc>${loc}</loc>`;
-  xml += `<lastmod>${lastmod}</lastmod>`;
-  xml += `<priority>${priority}</priority>`;
-  xml += `<changefreq>${changefreq}</changefreq>`;
-  xml += '</url>';
-
-  return xml;
-};
-
-app.get('/sitemap.xml', async (req, res) => {
-  const sitemap = await generateSiteMap();
-  res.header('Content-Type', 'text/xml');
-  res.send(sitemap);
-});
-
-// Serve static files from /browser
-app.get(
-  '*.*',
-  express.static(DIST_FOLDER, {
-    maxAge: '1y',
-  }),
-);
-
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
-
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+export * from './src/main.server';

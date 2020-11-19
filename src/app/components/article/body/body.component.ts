8ee3a948bb87df54ad3d069273aa08a6633906e9
Jacob Johnston <Methodician@tricias-mbp.lan>
2020-11-19 13:24:51 -0800

    Initial strides

--- src/app/components/article/body/body.component.ts
+++ src/app/components/article/body/body.component.ts
@@ -11,10 +11,17 @@ import { isPlatformBrowser } from '@angular/common';
 // Lots of quill libraries: https://github.com/quilljs/awesome-quill
 // Shared cursor display: https://github.com/reedsy/quill-cursors
 
+// Seems that body images and much else is auto-scrubbed by the editor!
 // e.g. old body with images:
-// <h2><strong>Step 1.</strong></h2><h4>Make Coffee.</h4><figure class="image"><img src="https://fire842915"></figure><p><br><strong>Step 2.</strong> Make Foam.&nbsp;</p><figure class="image"><img src="https://f3a908"></figure><p><br><strong>Step 3.</strong> Pour foam into coffee in intricate way that makes a cool pattern.&nbsp;</p><figure class="image"><img src="https://firebasestorage.googleapis.com/v0/b/cosourcerytest.appspot.com/o/articleBodyImages%2FYzkKh52JqqxPVAXwTqr9%2FrwuVHod?alt=media&amp;token=03932005-7c83-49c0-9ca3-1fd8e4bb8a0c"></figure><p><br><strong>Step 4.</strong> Done</p><figure class="image"><img src="https://firebasestorage.googleapis.com/v0/b/cosourcerytest.appspot.com/o/articleBodyImages%2FYzkKh52JqqxPVAXwTqr9%2FJlXDb2f?alt=media&amp;token=b7ccf902-cf56-4df5-8f97-8431892c1104"></figure>
-// e.g. nwa body with image:
-// <p>How about with an image?</p><p><img src="data:image/jpeg;base64,/9j/4gv4SUNDX1BST0Z7PPRRVNx1Z//Z"></p><p>THat's an image...</p>
+// <h2><strong>Step 1.</strong></h2><h4>Make Coffee.</h4><figure class="image"><img src="https://fire842915"></figure><p><br><strong>Step 2.</strong> Make Foam.&nbsp;</p><figure class="image"><img src="https://f3a908"></figure><p><br><strong>Step 3.</strong> Pour foam into coffee in intricate way that makes a cool pattern.&nbsp;</p><figure class="image"><img src="https://firebase=b8a0c"></figure><p><br><strong>Step 4.</strong> Done</p><figure class="image"><img src="https://firebasestorage.googleapis.com/v0/b/cosourcerytest.appspot.com/o/articleBodyImages%2FYzkKh52JqqxPVAXwTqr9%2FJlXDb2f?alt=media&amp;token=b7ccf902-cf56-4df5-8f97-8431892c1104"></figure>
+// e.g. new body with image:
+/*
+  <p>How about with an image?</p>
+  <p>
+    <img src="data:image/jpeg;base64,/9j/4gv4SUNDX1BST0Z7PPRRVNx1Z//Z">
+  </p>
+  <p>THat's an image...</p>
+*/
 @Component({
   selector: 'cos-body',
   templateUrl: './body.component.html',
